3f320872de463ed133b5eb571a94f91d07c1e6f4
Jared Peterson <53375196+Illusivebyte@users.noreply.github.com>
2023-09-01 13:43:53 -0400

    refactor: update plugin template to use new interfaces (#4)
    * refactor(template-class): swap to implement plugin interface instead
    
    * refactor: change ArrayList to List
    
    ---------
    
    Co-authored-by: codejedi365 <codejedi365@gmail.com>

--- src/main/java/org/yourorg/TemplatePlugin.java
+++ src/main/java/org/yourorg/TemplatePlugin.java
@@ -1,60 +1,51 @@
 package org.yourorg;
 
-import org.venatorlabs.rodeo.graph.Node;
-import org.venatorlabs.rodeo.graph.Relationship;
+import java.util.List;
+import java.util.Map;
 import org.venatorlabs.rodeo.plugin.RodeoPlugin;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import org.venatorlabs.rodeo.types.INode;
+import org.venatorlabs.rodeo.types.IRelationship;
 
 /**
  * A template for building a rodeo plugin.
  */
-public class TemplatePlugin extends RodeoPlugin {
+public class TemplatePlugin implements RodeoPlugin {
 
     private boolean docHook = true;
     private boolean nodeHook = true;
     private boolean relationshipHook = true;
 
-    @Override
     public String getDescription() {
         return "A template plugin";
     }
 
-    @Override
     public String getName() {
         return "template-plugin";
     }
 
-    @Override
-    public void processDoc(HashMap<String, Object> doc) {
+    public void processDoc(Map<String, Object> doc) {
         System.out.println("I process a document!");
         // TODO: your logic here
     }
 
-    @Override
-    public void processNodes(ArrayList<Node> nodes) {
+    public void processNodes(List<INode> nodes) {
         System.out.println("I process nodes!");
         // TODO: your logic here
     }
 
-    @Override
-    public void processRelationships(ArrayList<Relationship> rels) {
+    public void processRelationships(List<IRelationship> rels) {
         System.out.println("I process relationships!");
         // TODO: your logic here
     }
 
-    @Override
     public boolean isDocHook() {
         return docHook;
     }
 
-    @Override
     public boolean isNodeHook() {
         return nodeHook;
     }
 
-    @Override
     public boolean isRelationshipHook() {
         return relationshipHook;
     }
